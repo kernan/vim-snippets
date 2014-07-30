9c5f25cd4718a72802eb18da3cc4beba311810fa
Robert Kernan <rkernan@gmail.com>
2014-07-30 17:43:13 -0400

    Removed docstring generation.

--- pythonx/arg.py
+++ pythonx/arg.py
@@ -2,7 +2,11 @@ class Arg(object):
     def __init__(self, arg):
         self.arg = arg.split('=')[0].strip()
         self.type = ' '.join(self.arg.split()[0:-1])
-        self.name = self.arg.split()[-1].strip()
+        try:
+            self.name = self.arg.split()[-1].strip()
+        except IndexError:
+            self.name = ''
+            pass
 
     def __str__(self):
         return self.arg
@@ -10,24 +14,7 @@ class Arg(object):
     def __unicode__(self):
         return self.arg
 
+    @staticmethod
     def get_args(arglist):
         args = [Arg(arg) for arg in arglist.split(',') if arg]
         return args
-
-class PyArg(Arg):
-    def __init__(self, arg):
-        super(PyArg, self).__init__(arg)
-        self.__is_kwarg = '**' in self.arg
-
-    def is_kwarg(self):
-        return self.__is_kwarg
-
-    def get_args(arglist):
-        args = [PyArg(arg) for arg in arglist.split(',') if arg]
-        args = [arg for arg in args if arg.name != 'self']
-        return args
-
-if __name__ == '__main__':
-    arglist = 'brap, **kwargs'
-    args = PyArg.get_args(arglist)
-    for arg in args: print(arg.name + ", " + str(arg.is_kwarg()))
