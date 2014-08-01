4e0dde32de57d7cd3917c84aeeb7fcd96d3159c6
Robert Kernan <rkernan@gmail.com>
2014-08-01 01:56:39 -0400

    Added more name conversions and fixed naming conventions in C++.

--- pythonx/text.py
+++ pythonx/text.py
@@ -1,30 +1,21 @@
 import re
 
-def to_camel(s, upper = False):
+def to_camel_case(s):
     if len(s) == 0:
         return s
-    r = re.sub(r'(?!^)_([a-zA-Z])', lambda m: m.group(1).upper(), s)
-    if upper:
-        if len(s) == 1:
-            return r[0].upper()
-        else:
-            return r[0].upper() + r[1:]
-    else:
-        if len(s) == 1:
-            return r[0].lower()
-        else:
-            return r[0].lower() + r[1:]
+    s = s[0].lower() + s[1:]
+    return re.sub(r'(?!^)_([a-zA-Z])', lambda m: m.group(1).upper(), s)
 
-def to_snake(s):
+def to_pascal_case(s):
+    s = to_camelCase(s);
+    return s[0].upper() + s[1:]
+
+
+def to_snake_case(s):
     if len(s) == 0:
         return s
-    r = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', s)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', r).lower()
+    s = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', s)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s).lower()
 
-if __name__ == '__main__':
-    print(to_camel('getHTTPResponseCode'))
-    print(to_camel('getHTTPResponseCode', True))
-    print(to_camel('getHTTPResponseCode'))
-    print(to_camel('get_http_response_code', True))
-    print(to_snake('getHTTPResponseCode'))
-    print(to_snake('get_http_response_code'))
+def to_capitals_case(s):
+    return to_snake_case(s).upper()
